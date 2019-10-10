fdaba5c17894c8bf7e81f3bacbef5f1379ca1b98
szczypkamaciek <szczypka.maciek@gmail.com>
2019-10-10 16:11:58 +0200

    Updates

---
+++ precache-manifest.ee69989b07822b86cea123710d57f7e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b1c69ba006fb2f2c925a756c8b816b8",
+    "url": "/lotto/index.html"
+  },
+  {
+    "revision": "af53cabe85c2b9d17dcf",
+    "url": "/lotto/static/css/main.fe5d3b28.chunk.css"
+  },
+  {
+    "revision": "fca8d2fdffb8d298f3dd",
+    "url": "/lotto/static/js/2.1a3d16a8.chunk.js"
+  },
+  {
+    "revision": "af53cabe85c2b9d17dcf",
+    "url": "/lotto/static/js/main.1a1305f9.chunk.js"
+  },
+  {
+    "revision": "83e7e362b8cdd0028e66",
+    "url": "/lotto/static/js/runtime-main.9e38bf01.js"
+  }
+]);
